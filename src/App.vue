594b15cd21bcf1906c82a755a3ddd30f2946d46d
jacky <jackywu526@gmail.com>
2023-04-12 10:11:39 +0800

    feat: 新增 版本控制 / feat: 版本號更新 0.1.1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import TxtFileReader from './components/TxtFileReader.vue'
 
-console.log(`版本號: 0.1.0`);
+console.log(`版本號: ${import.meta.env.VITE_APP_VERSION}`);
 </script>
 
 <template>
